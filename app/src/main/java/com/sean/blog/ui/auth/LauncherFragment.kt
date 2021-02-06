45e88b1f83ddc03f5787efb3d515c88e450573c4
sean <happylim99@gmail.com>
2021-02-06 13:10:53 +0800

    complete auth navigation

--- app/src/main/java/com/sean/blog/ui/auth/LauncherFragment.kt
+++ app/src/main/java/com/sean/blog/ui/auth/LauncherFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 
 import com.sean.blog.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class LauncherFragment : Fragment() {
 
     override fun onCreateView(
@@ -19,6 +19,37 @@ class LauncherFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        register.setOnClickListener {
+            Toast.makeText(context, "aaaaaaaaaaa", Toast.LENGTH_LONG).show()
+            navRegistration()
+        }
+
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        forgot_password.setOnClickListener {
+            navForgotPassword()
+        }
+
+        focusable_view.requestFocus()
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegistration() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
     }
 
 }
---
+++ app/src/main/java/com/sean/blog/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sean.blog.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity{
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_launcher)
+//    }
+}
